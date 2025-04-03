3dd8b40d795ef6b446877ce78e0fef7f4356bda5
Liza Merzh <elizabeth.mer6@gmail.com>
2025-04-03 20:44:16 +0300

    test: exclude Kafka auto-configuration in unit tests

--- worker/src/test/java/com/example/notificationapp/worker/WorkerApplicationTests.java
+++ worker/src/test/java/com/example/notificationapp/worker/WorkerApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 
-@SpringBootTest
-@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+@EmbeddedKafka(partitions = 1)
+@SpringBootTest(properties = "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}")
 class WorkerApplicationTests {
 
 	@Test
