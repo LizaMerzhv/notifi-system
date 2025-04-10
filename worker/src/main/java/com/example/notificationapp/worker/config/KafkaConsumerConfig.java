6c5edf629fbfbdc4f2941a62ca3cab230467389d
Liza Merzh <elizabeth.mer6@gmail.com>
2025-04-10 19:20:23 +0300

    fix: resolve Kafka startup issue in local environment

--- worker/src/main/java/com/example/notificationapp/worker/config/KafkaConsumerConfig.java
+++ worker/src/main/java/com/example/notificationapp/worker/config/KafkaConsumerConfig.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConsumerConfig {
 
-    @Value("${kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
     @Bean
